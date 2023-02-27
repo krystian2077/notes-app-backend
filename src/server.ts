ec5a1f157b9a13ade5eb8973cb66793d7ae24071
Krystian <krystian.potaczek07@gmail.com>
2023-02-27 21:41:30 +0100

    Connect and configure MongoDb Atlas, add .env file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
+import env from "./utils/validateEnv";
+import mongoose from "mongoose";
 import express from "express";
 
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!!");
 });
 
-app.listen(port, () => {
-  console.log("Server running on port http://localhost:" + port);
-});
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_LINK)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port http://localhost:" + port);
+    });
+  })
+  .catch(console.error);
---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,6 @@
+import { cleanEnv, port, str } from "envalid";
+
+export default cleanEnv(process.env, {
+  MONGO_CONNECTION_LINK: str(),
+  PORT: port(),
+});
